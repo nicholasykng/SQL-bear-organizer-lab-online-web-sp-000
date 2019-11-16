7862ced33e921e7e9e931f701794d5b71d90d52a
Nicholas Ng <nicholasykng@gmail.com>
2019-11-16 23:40:21 +0000

    Automatically backed up by Learn

---
+++ lib/insert.sql
@@ -0,0 +1,8 @@
+INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ("Mr. Chocolate", 23, "M", "Brown", "Social", "Alive");
+INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ("Rowdy", 13, "M", "orange", "Funny", "Alive");
+INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ("Tabitha", 24, "F", "Yellow", "Wise", "Alive");
+INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ("Sergeant Brown");
+INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ("Melissa");
+INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ("Grinch");
+INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ("Wendy");
+INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES (NULL, );
